124533f911cbb209c37210d06417002b8540118e
vasu <bluesky04286@gmail.com>
2024-06-17 18:12:30 +0530

    Implement home screen

---
+++ src/app/layouts/app-bar/app-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-bar",
+  templateUrl: "app-bar.component.html",
+  styleUrls: ["./app-bar.component.scss"],
+  standalone: true,
+})
+export class AppbarComponent {}
--- src/app/screens/home/home.screen.ts
+++ src/app/screens/home/home.screen.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
-
+import {
+  Component,
+  TemplateRef,
+  ViewChild,
+  computed,
+  model,
+  signal,
+} from "@angular/core";
+import { AppbarComponent } from "../../layouts/app-bar/app-bar.component";
+import { Dialog, DialogModule } from "@angular/cdk/dialog";
 @Component({
-    selector: 'app-home',
-    templateUrl: 'home.screen.html',
-    styleUrls: ["./home.screen.scss"],
-    standalone: true
+  selector: "app-home",
+  templateUrl: "home.screen.html",
+  styleUrls: ["./home.screen.scss"],
+  standalone: true,
+  imports: [AppbarComponent, DialogModule],
 })
-export class HomeScreenComponent  {
-    
+export class HomeScreenComponent {
+  @ViewChild("resetDialog")
+  resetDialog!: TemplateRef<any>;
+
+  constructor(public dialog: Dialog) {}
+
+  readonly roundCounter = computed(() => this._counter() % this.threshold());
+
+  readonly totalRound = computed(() =>
+    Math.floor(this._counter() / this.threshold())
+  );
+
+  private readonly _counter = signal(0);
+
+  threshold = model(5); // acting like input() + signal
+
+  onRoundCounterClick() {
+    this._counter.update((val) => val + 1);
+  }
+
+  onReset() {
+    this.dialog.open(this.resetDialog, {
+      minWidth: "300px",
+    });
+  }
 }
