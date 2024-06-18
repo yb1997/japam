84765173365418cb84eff35e23b04afbf58fc072
vasu <bluesky04286@gmail.com>
2024-06-18 15:48:15 +0530

    Implement reset dialog

--- src/app/screens/home/home.screen.ts
+++ src/app/screens/home/home.screen.ts
@@ -7,7 +7,14 @@ import {
   signal,
 } from "@angular/core";
 import { AppbarComponent } from "../../layouts/app-bar/app-bar.component";
-import { Dialog, DialogModule } from "@angular/cdk/dialog";
+import { Dialog, DialogModule, DialogRef } from "@angular/cdk/dialog";
+
+import { Preferences } from '@capacitor/preferences';
+import { App } from '@capacitor/app';
+
+
+type CounterName = "Current Round" | "Total Rounds";
+
 @Component({
   selector: "app-home",
   templateUrl: "home.screen.html",
@@ -19,25 +26,48 @@ export class HomeScreenComponent {
   @ViewChild("resetDialog")
   resetDialog!: TemplateRef<any>;
 
+  dialogRef: DialogRef | null = null;
+
+  counterList: CounterName[] = ["Current Round", "Total Rounds"];
+
   constructor(public dialog: Dialog) {}
 
-  readonly roundCounter = computed(() => this._counter() % this.threshold());
+  // readonly roundCounter = computed(() => this._counter() % this.threshold());
 
-  readonly totalRound = computed(() =>
-    Math.floor(this._counter() / this.threshold())
-  );
+  // readonly totalRound = computed(() =>
+  //   Math.floor(this._counter() / this.threshold())
+  // );
+
+  readonly roundCounter = signal(0);
+  readonly totalRound = signal(0);
 
   private readonly _counter = signal(0);
 
-  threshold = model(5); // acting like input() + signal
+  threshold = 5; // acting like input() + signal
 
   onRoundCounterClick() {
     this._counter.update((val) => val + 1);
+
+    this.totalRound.update((val) =>
+      this.roundCounter() + 1 === this.threshold ? val + 1 : val
+    );
+    this.roundCounter.update((val) => this._counter() % this.threshold);
   }
 
-  onReset() {
-    this.dialog.open(this.resetDialog, {
+  showResetDialog() {
+    this.dialogRef = this.dialog.open(this.resetDialog, {
       minWidth: "300px",
     });
+  }
+
+  onReset(counterName: CounterName) {
+    if (counterName === "Total Rounds") {
+      this.totalRound.set(0);
+    }
+
+    this._counter.set(0);
+    this.roundCounter.set(0);
+
+    this.dialogRef?.close();
   }
 }
